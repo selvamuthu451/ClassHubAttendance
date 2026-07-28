5d86134a192cb67b58b9fb03a3c2919d0e6c66fa
selvamuthu451 <meenuushiva@gmail.com>
2026-07-28 17:54:10 +0530

    Use Railway Mysql

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 DB_CONFIG = {
-    "host": os.environ.get("MYSQLHOST"),
-    "user": os.environ.get("MYSQLUSER"),
-    "password": os.environ.get("MYSQLPASSWORD"),
-    "database": os.environ.get("MYSQLDATABASE"),
-    "port": int(os.environ.get("MYSQLPORT", 3306))
+    "host": os.getenv("MYSQLHOST"),
+    "user": os.getenv("MYSQLUSER"),
+    "password": os.getenv("MYSQLPASSWORD"),
+    "database": os.getenv("MYSQLDATABASE"),
+    "port": int(os.getenv("MYSQLPORT"))
 }
